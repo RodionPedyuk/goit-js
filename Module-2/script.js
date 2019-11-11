a680deca17c08dff9dee65466947e1c970f9c8e5
Rodion Pedyuk <rodionpedyuk@gmail.com>
2019-11-11 16:04:38 +0200

    fix

--- Module-2/script.js
+++ Module-2/script.js
@@ -66,17 +66,17 @@
 
 // --------------TASK 5------------------
 
-// let str = prompt("Enter your sentence...");
+let str = prompt("Enter your sentence...").toLowerCase();
 
-// const checkForSpam = function(str) {
-//   if (str.indexOf("sale") >= 0 || str.indexOf("spam") >= 0) {
-//     console.log(true);
-//   } else {
-//     console.log(false);
-//   }
-// };
+const checkForSpam = function(str) {
+  if (str.indexOf("sale") >= 0 || str.indexOf("spam") >= 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
 
-// checkForSpam(str);
+checkForSpam(str);
 
 // ---------------------------------------
 
@@ -99,4 +99,3 @@
 //   total += num;
 // }
 // alert(`Общая сумма чисел равна ${total}`);
-
